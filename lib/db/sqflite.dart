649ef27573e021b0a9e39656d9a6722e8a8fdb34
Hajung Song <dongoc21hj@gmail.com>
2023-05-30 20:32:27 +0900

    expense 메소드 생성

--- lib/db/sqflite.dart
+++ lib/db/sqflite.dart
@@ -1,5 +1,6 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:trip_expenses_manager/models/expense.dart';
 import 'package:trip_expenses_manager/models/trip.dart';
 
 class TripDatabase {
@@ -24,7 +25,15 @@ class TripDatabase {
         id INTEGER PRIMARY KEY,
         title TEXT NOT NULL,
         start_date DATE NOT NULL,
-        end_date DATE
+        end_date DATE;
+        
+      CREATE TABLE IF NOT EXISTS expenses (
+        id INTEGER PRIMARY KEY,
+        trip_id INTEGER NOT NULL,
+        description TEXT NOT NULL,
+        amount INTEGER NOT NULL,
+        date_time DATE NOT NULL,
+        category TEXT,
       )
     ''');
   }
@@ -39,7 +48,7 @@ class TripDatabase {
     return trip;
   }
 
-  Future<Trip> updateTrip(trip) async {
+  Future<Trip> updateTrip(Trip trip) async {
     final db = await _openDb();
     await db.update(
       'trips',
@@ -83,5 +92,54 @@ class TripDatabase {
         endDate: maps[i]['end_date'],
       );
     });
+  }
+
+  Future<Expense> getExpense(int id) async {
+    final db = await _openDb();
+    final List<Map<String, dynamic>> maps = await db.query(
+      'expenses',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    return Expense.fromJson(maps[0]);
+  }
+
+  Future<List<Expense>> getExpenseList(int tripId) async {
+    final db = await _openDb();
+    final List<Map<String, dynamic>> maps =
+        await db.query('expenses', where: 'trip_id = ?', whereArgs: [tripId]);
+
+    return List.generate(maps.length, (i) {
+      return Expense(
+        id: maps[i]['id'],
+        tripId: maps[i]['trip_id'],
+        description: maps[i]['description'],
+        amount: maps[i]['amount'],
+        dateTime: maps[i]['date_time'],
+        category: maps[i]['category'],
+      );
+    });
+  }
+
+  Future<Expense> updateExpense(Expense expense) async {
+    final db = await _openDb();
+    await db.update(
+      'expenses',
+      expense.toMap(),
+      where: 'id = ?',
+      whereArgs: [expense.id],
+    );
+    return expense;
+  }
+
+  Future<int> deleteExpense(int id) async {
+    final db = await _openDb();
+    await db.delete(
+      'expenses',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    return id;
   }
 }
